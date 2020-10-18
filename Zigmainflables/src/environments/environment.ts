d3acdcf270c9459fd4f7f6d04325a332dad8467f
daniel <zigmainflables@gmail.com>
2020-10-18 01:23:04 -0500

    se creo conexcion con firebase

---
+++ Zigmainflables/src/environments/environment.ts
@@ -0,0 +1,14 @@
+
+export const environment = {
+  production: false,
+  firebase:{
+  apiKey: "AIzaSyC_kg6-zA60Fup9wpj3z6V8ZvGuwPal7xU",
+  authDomain: "zigmainflables-fabrica.firebaseapp.com",
+  databaseURL: "https://zigmainflables-fabrica.firebaseio.com",
+  projectId: "zigmainflables-fabrica",
+  storageBucket: "zigmainflables-fabrica.appspot.com",
+  messagingSenderId: "732813270799",
+  appId: "1:732813270799:web:21bae09191cc8b824e687a",
+  measurementId: "G-G3SC1N58X9"
+  }
+  };
