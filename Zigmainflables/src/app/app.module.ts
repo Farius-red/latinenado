2f98ecd7a4fbeddc989adbae7458733c978583bb
daniel <zigmainflables@gmail.com>
2020-10-19 00:57:52 -0500

    agrege roting y nuevos componentes

--- Zigmainflables/src/app/app.module.ts
+++ Zigmainflables/src/app/app.module.ts
@@ -1,19 +1,54 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-
+import { Component, NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+
+//Dependencias fire base
+
 import{AngularFireModule} from 'angularfire2';
 import{environment} from '../environments/environment';
 import{AngularFirestoreModule} from 'angularfire2/firestore';
 import{AngularFireStorageModule} from 'angularfire2/storage';
 import{AngularFireAuthModule} from 'angularfire2/auth';
 
+
+//servicios 
+import { ConexionService} from './services/conexion.service';
+
+//Componentes//
+import { AppComponent } from './app.component';
+import { FooterComponent } from './componentes/footer/footer.component';
+import { NavComponent } from './componentes/nav/nav.component';
+import { LoginComponent } from './componentes/login/login.component';
+import { RegistroComponent } from './componentes/registro/registro.component';
+import { NosotrosComponent } from './componentes/nosotros/nosotros.component';
+import { InicioComponent } from './componentes/inicio/inicio.component';
+import { TiendaComponent } from './componentes/tienda/tienda.component';
+
+const routes: Routes=[
+   
+    { path: 'tienda', component: TiendaComponent},
+    { path: 'registro', component: RegistroComponent},
+    { path: 'login', component: LoginComponent},
+    { path: 'inicio', component: InicioComponent},
+    { path: 'nosotros', component: NosotrosComponent},
+    { path: '', component: InicioComponent , pathMatch:'full'},
+    { path: '**', redirectTo:'/', pathMatch:'full'}
+];
+ 
 @NgModule({
 declarations: [
-AppComponent
+AppComponent,
+FooterComponent,
+NavComponent,
+LoginComponent,
+RegistroComponent,
+NosotrosComponent,
+InicioComponent,
+TiendaComponent
 ],
 imports: [
+RouterModule.forRoot(routes),
 BrowserModule,
 AppRoutingModule,
 AngularFireModule.initializeApp(environment.firebase),
@@ -21,7 +56,9 @@ AngularFirestoreModule,
 AngularFireStorageModule,
 AngularFireAuthModule
 ],
-providers: [],
+providers: [
+    ConexionService
+],
 bootstrap: [AppComponent]
 })
 export class AppModule { }
